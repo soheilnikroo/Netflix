92052e919fed4ffdb20b42626aaf499a1d646eaf
Soheil Nikroo <soheiln1234@gmail.com>
2021-05-13 01:20:34 -0700

    setup Botoom Tab Navigator

---
+++ types.tsx
@@ -0,0 +1,24 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Coming_Soon: undefined;
+  Search: undefined;
+  Downloads: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
